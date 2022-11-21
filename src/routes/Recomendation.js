74eb63b370a01f09c78c21811a0d8a8aa0ad0bf1
Cbravor1991 <cbravor@fi.uba.ar>
2022-11-21 02:30:12 -0300

    diseño tarjetas recomendaciones por default

--- src/routes/Recomendation.js
+++ src/routes/Recomendation.js
@@ -2,35 +2,44 @@ import Logo from '../components/Logo';
 import { useNavigate, Link } from "react-router-dom";
 import '../Recomendation.module.css';
 import Slider from '../components/Slider';
-import data from "../dataRecomendation"
+import data from "../dataRecomendation";
+const images = require.context('../images/')
 
 
 
 const Recomendation = () => {
 
-    const slide = data.map(item=>{
-        return (
-            <Slider
-            key ={item.id}
-            {...item}
-            />
-        )
 
 
-   })
-    
-   
 
     return (
-          <div className={['contenedor-recomendaciones']} >
-                <div className={['slider']}> 
+        <div className={['contenedor-recomendaciones']} >
+            <div className={['transparencia']}>
+            <div className={['slider']}>
 
-                {slide}
-                
+                <div className="mar-del-plata">
+                    <img src={images(`./${data[0].coverImg}`)} className="card--image-mar-del" />
                 </div>
-        
-             </div>
-        )
+
+                <div className="bariloche">
+                    <img src={images(`./${data[1].coverImg}`)} className="card-bariloche" />
+                </div>
+
+                <div className="cordoba">
+                    <img src={images(`./${data[2].coverImg}`)} className="card-cordoba" />
+                </div>
+
+                <div className="san-martin-de-los-andes">
+                    <img src={images(`./${data[3].coverImg}`)} className="card-san-martin-los-andes" />
+                </div>
+
+
+
+            </div>
+            </div>
+
+        </div>
+    )
 
 }
 
